26fe2badc56f91e50a38e71b0c8e42fa171b995e
BaranovMykola <mapsg32@gmail.com>
2017-11-13 10:06:47 +0200

    Added empty entites for GlobalStatistics class

---
+++ KeyboardModel/Statistic/StatisticsIdentifier.cs
@@ -0,0 +1,11 @@
+﻿using KeyboardModel.Enums;
+
+namespace KeyboardModel.Statistic
+{
+    /// <summary>
+    /// Represent unique identifier in sense of three unemus <see cref="Complexity"/>, <see cref="Language"/>, <see cref="Time"/>
+    /// </summary>
+    public struct StatisticsIdentifier
+    {
+    }
+}

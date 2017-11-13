ad9062cc3a11989b6fc9f365e014ceabd23e1300
BaranovMykola <mapsg32@gmail.com>
2017-11-13 11:13:27 +0200

    Implemented bare GlobalStatistics class

--- KeyboardModel/Statistic/GlobalStatistics.cs
+++ KeyboardModel/Statistic/GlobalStatistics.cs
@@ -1,6 +1,14 @@
-﻿namespace KeyboardModel.Statistic
+﻿using System.Collections.Generic;
+
+namespace KeyboardModel.Statistic
 {
     public class GlobalStatistics
     {
+        public Dictionary<StatisticsIdentifier, ErrorStatistics> LocalStatistics { get; set; }
+
+        public GlobalStatistics()
+        {
+            LocalStatistics = new Dictionary<StatisticsIdentifier, ErrorStatistics>();
+        }
     }
 }

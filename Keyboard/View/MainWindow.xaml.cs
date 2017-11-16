340db71d94578beb11e03d3ddde0d3bb91ab39f3
BaranovMykola <mapsg32@gmail.com>
2017-11-16 22:07:30 +0200

    Moved views to their own folder. Moved behaviour to view/behaviour folder

---
+++ Keyboard/View/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace Keyboard.View
+{
+    using System.Windows;
+    using Keyboard.ViewModel;
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainWindowViewModel(new WindowMediator() { CreateWindow = () => new Settings() })
+            {
+                Settings = new SettingsViewModel()
+            };
+        }
+    }
+}
--- Keyboard/ViewModel/MainWindowViewModel.cs
+++ Keyboard/ViewModel/MainWindowViewModel.cs
@@ -7,7 +7,6 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
-using System.Windows.Interactivity;
 using Keyboard.Command;
 
 namespace Keyboard.ViewModel
@@ -76,39 +75,6 @@ namespace Keyboard.ViewModel
         private void KeyDown(object parameter)
         {
             Console.WriteLine($"key down -> [{parameter?.ToString()}]");
-        }
-    }
-
-    public class KeyUpWithArgsBehavior : Behavior<UIElement>
-    {
-        public ICommand KeyUpCommand
-        {
-            get { return (ICommand)GetValue(KeyUpCommandProperty); }
-            set { SetValue(KeyUpCommandProperty, value); }
-        }
-
-        public static readonly DependencyProperty KeyUpCommandProperty =
-            DependencyProperty.Register("KeyUpCommand", typeof(ICommand), typeof(KeyUpWithArgsBehavior), new UIPropertyMetadata(null));
-
-
-        protected override void OnAttached()
-        {
-            AssociatedObject.KeyUp += new KeyEventHandler(AssociatedObjectKeyUp);
-            base.OnAttached();
-        }
-
-        protected override void OnDetaching()
-        {
-            AssociatedObject.KeyUp -= new KeyEventHandler(AssociatedObjectKeyUp);
-            base.OnDetaching();
-        }
-
-        private void AssociatedObjectKeyUp(object sender, KeyEventArgs e)
-        {
-            if (KeyUpCommand != null)
-            {
-                KeyUpCommand.Execute(e.Key);
-            }
         }
     }
 }

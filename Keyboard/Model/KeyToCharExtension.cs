40ed677fb4f7e66822b0ea289df862a6c2df3fba
BaranovMykola <mapsg32@gmail.com>
2017-11-19 14:53:38 +0200

    Refactored KeyToCharExtension class

---
+++ Keyboard/Model/KeyToCharExtension.cs
@@ -0,0 +1,72 @@
+﻿namespace Keyboard.Model
+{
+    using System.Runtime.InteropServices;
+    using System.Text;
+    using System.Windows.Input;
+
+    internal static class KeyToCharExtension
+    {
+        public enum MapType : uint
+        {
+            MAPVK_VK_TO_VSC = 0x0,
+            MAPVK_VSC_TO_VK = 0x1,
+            MAPVK_VK_TO_CHAR = 0x2,
+            MAPVK_VSC_TO_VK_EX = 0x3,
+        }
+
+        [DllImport("user32.dll")]
+        private static extern int ToUnicode(
+            uint virtKey,
+            uint scanCode,
+            byte[] keyState,
+            [Out, MarshalAs(UnmanagedType.LPWStr, SizeParamIndex = 4)]
+                StringBuilder pwszBuff,
+            int cchBuff,
+            uint flags);
+
+        [DllImport("user32.dll")]
+        private static extern bool GetKeyboardState(byte[] keyState);
+
+        [DllImport("user32.dll")]
+        private static extern uint MapVirtualKey(uint code, MapType mapType);
+
+        /// <summary>
+        /// Converts virtual key to real char
+        /// </summary>
+        /// <param name="key">Input key</param>
+        /// <returns>Real char</returns>
+        public static char GetCharFromKey(this Key key)
+        {
+            char ch = ' ';
+
+            int virtualKey = KeyInterop.VirtualKeyFromKey(key);
+            byte[] keyboardState = new byte[256];
+            GetKeyboardState(keyboardState);
+
+            uint scanCode = MapVirtualKey((uint)virtualKey, MapType.MAPVK_VK_TO_VSC);
+            StringBuilder stringBuilder = new StringBuilder(2);
+
+            int result = ToUnicode((uint)virtualKey, scanCode, keyboardState, stringBuilder, stringBuilder.Capacity, 0);
+            switch (result)
+            {
+                case -1:
+                    break;
+                case 0:
+                    break;
+                case 1:
+                {
+                    ch = stringBuilder[0];
+                    break;
+                }
+
+                default:
+                {
+                    ch = stringBuilder[0];
+                    break;
+                }
+            }
+
+            return ch;
+        }
+    }
+}
--- Keyboard/ViewModel/MainWindowViewModel.cs
+++ Keyboard/ViewModel/MainWindowViewModel.cs
@@ -1,5 +1,4 @@
-﻿using System.Runtime.InteropServices;
-using System.Text;
+﻿using Keyboard.Model;
 
 namespace Keyboard.ViewModel
 {
@@ -63,67 +62,9 @@ namespace Keyboard.ViewModel
 
         private void KeyDown(object parameter)
         {
+            var key = (Key) parameter;
             Console.WriteLine($"key down -> [{parameter?.ToString()}]");
-            Console.WriteLine(H.GetCharFromKey((Key) parameter));
-        }
-    }
-
-    class H
-    {
-        public enum MapType : uint
-        {
-            MAPVK_VK_TO_VSC = 0x0,
-            MAPVK_VSC_TO_VK = 0x1,
-            MAPVK_VK_TO_CHAR = 0x2,
-            MAPVK_VSC_TO_VK_EX = 0x3,
-        }
-
-        [DllImport("user32.dll")]
-        public static extern int ToUnicode(
-            uint wVirtKey,
-            uint wScanCode,
-            byte[] lpKeyState,
-            [Out, MarshalAs(UnmanagedType.LPWStr, SizeParamIndex = 4)]
-            StringBuilder pwszBuff,
-            int cchBuff,
-            uint wFlags);
-
-        [DllImport("user32.dll")]
-        public static extern bool GetKeyboardState(byte[] lpKeyState);
-
-        [DllImport("user32.dll")]
-        public static extern uint MapVirtualKey(uint uCode, MapType uMapType);
-
-        public static char GetCharFromKey(Key key)
-        {
-            char ch = ' ';
-
-            int virtualKey = KeyInterop.VirtualKeyFromKey(key);
-            byte[] keyboardState = new byte[256];
-            GetKeyboardState(keyboardState);
-
-            uint scanCode = MapVirtualKey((uint)virtualKey, MapType.MAPVK_VK_TO_VSC);
-            StringBuilder stringBuilder = new StringBuilder(2);
-
-            int result = ToUnicode((uint)virtualKey, scanCode, keyboardState, stringBuilder, stringBuilder.Capacity, 0);
-            switch (result)
-            {
-                case -1:
-                    break;
-                case 0:
-                    break;
-                case 1:
-                    {
-                        ch = stringBuilder[0];
-                        break;
-                    }
-                default:
-                    {
-                        ch = stringBuilder[0];
-                        break;
-                    }
-            }
-            return ch;
+            Console.WriteLine(key.GetCharFromKey());
         }
     }
 }
